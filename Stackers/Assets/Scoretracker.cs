89044fe037dbc771d5f8727ff1187877e9bd7201
Shaunzafar28 <55443382+Shaunzafar28@users.noreply.github.com>
2020-03-13 08:41:11 -0400

    Made GameManager

---
+++ Stackers/Assets/Scoretracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoretracker : MonoBehaviour
+{
+    public ScoreTracker INSTANCE;
+    public int bestscore = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = this;
+            bestscore = 0;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
